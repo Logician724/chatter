26a67f2081ccc913db6945915115c7d91d439d84
Logician724 <reda.i@eseed.net>
2019-04-11 01:28:57 +0200

    feat: Add guarded attr array to category model

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $table = 'chatter_categories';
+    protected $guarded = [];
     public $timestamps = true;
     public $with = 'parents';
+    
 
     public function discussions()
     {
